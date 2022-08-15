b5247a8fb135c763b3fc4dbaba83c829de7e948e
SHWAN108 <shwanshero888@gmail.com>
2022-08-15 15:58:04 +0300

    adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
+import {BrowserRouter , Route , Routes} from 'react-router-dom' 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
   return (
     <div className="App">
-      <h1 className=""> hello world</h1>
+      <div className=' bg-red-500'>hello every 0ne</div>
+    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/regester' element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
