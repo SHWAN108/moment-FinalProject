4c173e6b5ff1e285f4967c2321cf39860607a18a
SHWAN108 <shwanshero888@gmail.com>
2022-08-17 16:28:01 +0300

    creating login ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import {BrowserRouter , Route , Routes} from 'react-router-dom' 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
     <div className="App">
-      <div className=' bg-red-500'>hello every 0ne</div>
-    
+      <div className=" "></div>
+
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/regester' element={<Register/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/regester" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="bg-sky-500 h-screen ">
+      <Link
+        to="/"
+        className=" m-auto flex justify-center w-fit text-white text-6xl font-bold pt-5 hover:text-sky-300 hover:text-7xl transition-color duration-200"
+      >
+        moment
+      </Link>
+      <div className="items-center  justify-center flex mt-24">
+        <div className=" w-96 flex flex-col bg-white text-center rounded-xl pb-20">
+          <h1 className=" text-4xl  pt-14 text-sky-500 font-bold">LOGIN</h1>
+          <input
+            type="email"
+            placeholder="email"
+            className=" border border-gray-300 h-10 rounded-sm focus: border-gray-500 pl-5 mx-5 mt-10"
+          ></input>
+          <input
+            type="password"
+            placeholder="password"
+            className=" border border-gray-300 h-10 rounded-sm focus: border-gray-500 pl-5 mx-5 mt-4 "
+          ></input>
+          <div className=" flex justify-end">
+            <button
+              type="submit"
+              className=" bg-sky-500 h-10 rounded-md text-white px-10 mt-5 mr-5 "
+            >
+              LOGIN
+            </button>
+          </div>
+          <Link
+            to="/regester"
+            className=" text-sm w-fit flex justify-start mt-5 ml-10 text-sky-500 hover:text-black"
+          >
+            Click here to register
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
