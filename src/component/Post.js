f21bcfc2ec7461a0ef39f3b1d193ca38aa0531d7
SHWAN108 <shwanshero888@gmail.com>
2022-09-14 11:26:13 +0300

    creating home page

---
+++ src/component/Post.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { AiFillHeart, AiOutlineComment } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+function Post({ post }) {
+  const navigate = useNavigate();
+  const getUserName = () => {
+    const email = post.user.email;
+    const username = email.substring(0, email.length - 10);
+    return username;
+  };
+
+  return (
+    <div onClick={() => navigate(`post/${post.id}`)} className="cursor-pointer">
+      <div className="flex item items-center card-sm p-2 bg-sky-500">
+        <div className=" h-10 w-10 rounded-full bg-white flex justify-center items-center text-sky-500 mr-2">
+          <span className=" text-2xl">{getUserName()[0]}</span>
+        </div>
+        <span className=" text-white">{getUserName()}</span>
+      </div>
+      <div className=" w-full text-center flex justify-center card-sm">
+        <img src={post.imageURL} alt="" className="h-60 w-60 " />
+      </div>
+      <div className=" card-sm p-2 flex w-full space-x-5 bg-sky-500">
+        <div className=" flex space-x-2 items-center">
+          <AiFillHeart size={25} className="fill-white" />
+          <h1 className=" text-white">{post.likes.length}</h1>
+        </div>
+        <div className=" flex space-x-2 items-center">
+          <AiOutlineComment size={25} className="fill-white" />
+          <h1 className=" text-white">{post.comment.length}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import DefaultLayout from "../component/DefaultLayout";
-
+import Post from "../component/Post";
+import { fireDB } from "../firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 function Home() {
+  const [data, setData] = useState([]);
+  const dispach = useDispatch();
+  const getData = async () => {
+    dispach({ type: "showLoading" });
+    const querySnapshot = await getDocs(collection(fireDB, "posts"));
+    const temp = [];
+    querySnapshot.forEach((doc) => {
+      temp.push({ ...doc.data(), id: doc.id });
+    });
+    setData(temp);
+    dispach({ type: "hideLoading" });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <DefaultLayout>
-      <div>Home</div>
+      <div className=" grid grid-cols-4 md:grid-cols-1">
+        {data.map((post) => {
+          return <Post post={post} />;
+        })}
+      </div>
     </DefaultLayout>
   );
 }
